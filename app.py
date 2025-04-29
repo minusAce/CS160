23edcaecdd77ff898e9595064d9b3f0969f25239
-Ace <143662232+minusAce@users.noreply.github.com>
2025-04-28 20:58:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,89 +1,119 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_login import UserMixin, LoginManager, login_required, login_user
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'CS160Project'
 
+bcrypt = Bcrypt(app)
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://Rex:abcd123@cluster0.7xpphgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["user_auth"]
+users_collection = db["users"]
+
+# Flask-Login setup
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+
+# User class
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data['_id'])
+        self.username = user_data['username']
+        self.password = user_data['password']
+
+    @staticmethod
+    def get_by_username(username):
+        user_data = users_collection.find_one({"username": username})
+        if user_data:
+            return User(user_data)
+        return None
+
+    @staticmethod
+    def get_by_id(user_id):
+        user_data = users_collection.find_one({"_id": ObjectId(user_id)})
+        if user_data:
+            return User(user_data)
+        return None
+
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
-
-db = SQLAlchemy(app)
+    return User.get_by_id(user_id)
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-
-
-
+# Forms
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
-
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)],
+                             render_kw={"placeholder": "Password"})
     submit = SubmitField("Register")
 
     def validate_username(self, username):
-        existing_user = User.query.filter_by(username=username.data).first()
-        if existing_user:
+        if users_collection.find_one({"username": username.data}):
             raise ValidationError("That username already exists. Please choose a different one.")
 
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
 
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)],
+                             render_kw={"placeholder": "Password"})
     submit = SubmitField("Login")
 
 
+# Routes
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if user.password == form.password.data:
-                login_user(user)
-                return redirect(url_for('dashboard'))
+        user = User.get_by_username(form.username.data)
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
-
-@app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        new_user = User(username=form.username.data, password=form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        users_collection.insert_one({
+            "username": form.username.data,
+            "password": hashed_password
+        })
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-with app.app_context():
-    try:
-        db.create_all()  # Create the tables
-        print("Tables created!")
-    except Exception as e:
-        print(f"Error creating tables: {e}")
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
